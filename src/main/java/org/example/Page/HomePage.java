2612b122571924c32f5fa08c806af4b2d544820c
Baganna Kumbar <bagannak@testvagrant.com>
2023-07-26 16:51:44 +0530

    TV

--- src/main/java/org/example/Page/HomePage.java
+++ src/main/java/org/example/Page/HomePage.java
@@ -10,8 +10,9 @@ public class HomePage {
     public HomePage(WebDriver webDriver) {
     }
     public void search(String searchItem) {
-    }
+    } 
     public List<Item> getSearchItems() {
+
         return new ArrayList<>();
     }
 
